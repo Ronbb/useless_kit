5fd346e91b28fd3bb9e138f3dc1f8406252fd3d1
ronbb <ronbiaobiao@gmail.com>
2022-09-30 10:19:25 +0800

    feat: implement RSA-OAEP-SHA256

--- lib/pages/converter/crypto.dart
+++ lib/pages/converter/crypto.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:cryptography/cryptography.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pointycastle/export.dart' hide State;
+import 'package:pointycastle/pointycastle.dart';
 import 'package:useless_kit/pages/converter/converter.dart';
 import 'package:useless_kit/pages/home/delegate.dart';
 
@@ -30,6 +33,7 @@ class _CryptoPageState extends State<CryptoPage> {
     const AlgorithmAesCbc128(),
     const AlgorithmAesCbc192(),
     const AlgorithmAesCbc256(),
+    const AlgorithmRsaOaepSha256(),
   ];
 
   Algorithm get algorithm {
@@ -91,8 +95,13 @@ abstract class Algorithm {
   Future<ConverterData> decrypt(ConverterData data);
 
   Future<ConverterData> encrypt(ConverterData data);
+
+  bool get enableDecryption => false;
+
+  bool get enableEncryption => false;
 }
 
+// for library cryptography AES
 abstract class AlgorithmCipher extends Algorithm {
   const AlgorithmCipher();
 
@@ -101,6 +110,12 @@ abstract class AlgorithmCipher extends Algorithm {
   List<int> get nonce => List.filled(algorithm.nonceLength, 0);
 
   String get kKey => 'Key';
+
+  @override
+  bool get enableDecryption => true;
+
+  @override
+  bool get enableEncryption => true;
 
   @override
   List<ConverterExtraItem> get extraDecodedItems => [
@@ -139,6 +154,149 @@ abstract class AlgorithmCipher extends Algorithm {
     );
 
     return data.copyWith(encoded: base64.encode(secretBox.cipherText));
+  }
+}
+
+// for library pointycastle RSA
+abstract class AlgorithmCipher2 extends Algorithm {
+  const AlgorithmCipher2();
+
+  AsymmetricBlockCipher get algorithm;
+
+  String get kPublicKey => 'PublicKey';
+
+  String get kPrivateKey => 'PrivateKey';
+
+  @override
+  bool get enableEncryption => true;
+
+  @override
+  bool get enableDecryption => true;
+
+  @override
+  List<ConverterExtraItem> get extraDecodedItems => [
+        ConverterExtraItem(
+          key: kPublicKey,
+          label: const Text('PublicKey'),
+        ),
+        ConverterExtraItem(
+          key: kPrivateKey,
+          label: const Text('PrivateKey'),
+        ),
+      ];
+
+  @override
+  Future<ConverterData> encrypt(ConverterData data) async {
+    final key = data.extraDecodedData[kPublicKey]!;
+    final publicKey = RSAKeyParser().parse(key) as RSAPublicKey;
+
+    algorithm.reset();
+    algorithm.init(true, PublicKeyParameter<RSAPublicKey>(publicKey));
+    final encrypted = algorithm.process(
+      Uint8List.fromList(
+        utf8.encode(data.decoded),
+      ),
+    );
+
+    return data.copyWith(encoded: base64.encode(encrypted));
+  }
+
+  @override
+  Future<ConverterData> decrypt(ConverterData data) async {
+    final key = data.extraDecodedData[kPublicKey]!;
+    final privateKey = RSAKeyParser().parse(key) as RSAPrivateKey;
+
+    algorithm.reset();
+    algorithm.init(false, PrivateKeyParameter<RSAPrivateKey>(privateKey));
+    final decrypted = algorithm.process(
+      Uint8List.fromList(
+        utf8.encode(data.decoded),
+      ),
+    );
+
+    return data.copyWith(encoded: base64.encode(decrypted));
+  }
+}
+
+/// RSA PEM parser.
+class RSAKeyParser {
+  static RSAAsymmetricKey parseFromString(String key) {
+    RSAKeyParser rsaKeyParser = RSAKeyParser();
+    return rsaKeyParser.parse(key);
+  }
+
+  /// Parses the PEM key no matter it is public or private, it will figure it out.
+  RSAAsymmetricKey parse(String key) {
+    final rows = key.split(RegExp(r'\r\n?|\n'));
+    final header = rows.first;
+
+    if (header == '-----BEGIN RSA PUBLIC KEY-----') {
+      return _parsePublic(_parseSequence(rows));
+    }
+
+    if (header == '-----BEGIN PUBLIC KEY-----') {
+      return _parsePublic(_pkcs8PublicSequence(_parseSequence(rows)));
+    }
+
+    if (header == '-----BEGIN RSA PRIVATE KEY-----') {
+      return _parsePrivate(_parseSequence(rows));
+    }
+
+    if (header == '-----BEGIN PRIVATE KEY-----') {
+      return _parsePrivate(_pkcs8PrivateSequence(_parseSequence(rows)));
+    }
+
+    throw FormatException('Unable to parse key, invalid format.', header);
+  }
+
+  /// 0 modulus(n), 1 publicExponent(e)
+  RSAAsymmetricKey _parsePublic(ASN1Sequence sequence) {
+    final List<ASN1Integer> asn1IntList =
+        sequence.elements!.cast<ASN1Integer>();
+    final modulus = asn1IntList.elementAt(0).integer;
+    final exponent = asn1IntList.elementAt(1).integer;
+    return RSAPublicKey(modulus!, exponent!);
+  }
+
+  /// 0 version, 1 modulus(n), 2 publicExponent(e), 3 privateExponent(d), 4 prime1(p), 5 prime2(q)
+  /// 6 exponent1(d mod (p-1)), 7 exponent2 (d mod (q-1)), 8 coefficient
+  RSAAsymmetricKey _parsePrivate(ASN1Sequence sequence) {
+    final List<ASN1Integer> asn1IntList =
+        sequence.elements!.cast<ASN1Integer>();
+    final modulus = asn1IntList.elementAt(1).integer;
+    final exponent = asn1IntList.elementAt(3).integer;
+    final p = asn1IntList.elementAt(4).integer;
+    final q = asn1IntList.elementAt(5).integer;
+    return RSAPrivateKey(modulus!, exponent!, p, q);
+  }
+
+  ASN1Sequence _parseSequence(List<String> rows) {
+    final keyText = rows
+        .skipWhile((row) => row.startsWith('-----BEGIN'))
+        .takeWhile((row) => !row.startsWith('-----END'))
+        .map((row) => row.trim())
+        .join('');
+
+    final keyBytes = Uint8List.fromList(base64.decode(keyText));
+    final asn1Parser = ASN1Parser(keyBytes);
+
+    return asn1Parser.nextObject() as ASN1Sequence;
+  }
+
+  ASN1Sequence _pkcs8PublicSequence(ASN1Sequence sequence) {
+    final ASN1Object bitString = sequence.elements![1];
+    final bytes = bitString.valueBytes!.sublist(1);
+    final parser = ASN1Parser(Uint8List.fromList(bytes));
+
+    return parser.nextObject() as ASN1Sequence;
+  }
+
+  ASN1Sequence _pkcs8PrivateSequence(ASN1Sequence sequence) {
+    final ASN1Object bitString = sequence.elements![2];
+    final bytes = bitString.valueBytes!;
+    final parser = ASN1Parser(bytes);
+
+    return parser.nextObject() as ASN1Sequence;
   }
 }
 
@@ -188,4 +346,16 @@ class AlgorithmAesGcm128 extends AlgorithmCipher {
 
   @override
   final String name = 'AES-GCM-128';
+}
+
+class AlgorithmRsaOaepSha256 extends AlgorithmCipher2 {
+  const AlgorithmRsaOaepSha256();
+
+  static final _algorithm = OAEPEncoding(RSAEngine());
+
+  @override
+  AsymmetricBlockCipher get algorithm => _algorithm;
+
+  @override
+  final String name = 'RSA-OAEP-SHA256';
 }
